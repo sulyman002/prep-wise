bcde8f13fbdc68844b1aebc9e8161eebd8075a3c
sulyman002 <oyedelesulaiman@gmail.com>
2025-08-19 15:18:02 +0100

    chp

--- src/components/PostReview/LogoAndProfile/LogoAndProfile.jsx
+++ src/components/PostReview/LogoAndProfile/LogoAndProfile.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 import prep_logo from "../../assets/prep_wise_logo.svg";
-import profile_icon from "../../assets/profile_image.png"
-
+import profile_icon from "../../assets/profile_image.png";
+import { useNavigate } from "react-router-dom";
 export const LogoAndProfile = () => {
+  const navigate = useNavigate();
   return (
     <div className="flex items-center justify-between w-full">
-      <div className="flex items-center justify-center gap-2 py-4 cursor-pointer">
+      <div onClick={() => {
+        navigate("/dashboard")
+      }} className="flex items-center justify-center gap-2 py-4 cursor-pointer">
         <img src={prep_logo} alt="" />
         <p className="font-600 font-semibold text-[20px] md:text-[32px] leading-[100%] text-[#DDDFFF]">
           PrepWise
         </p>
       </div>
       <div className="h-[50px] w-[50px] rounded-full cursor-pointer">
-        <img src={profile_icon} alt="" className="rounded-full h-[40px] w-[40px] md:h-[50px] md:w-[50px] "/>
+        <img
+          src={profile_icon}
+          alt=""
+          className="rounded-full h-[40px] w-[40px] md:h-[50px] md:w-[50px] "
+        />
       </div>
     </div>
   );
